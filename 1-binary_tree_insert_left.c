6c23e864a00000287af7bc5c70a8f2a10260d90e
Tonycrz <tonybaidoo0@gmail.com>
2022-02-17 10:40:10 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,26 +12,42 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *left_insert = malloc(sizeof(binary_tree_t));
 	binary_tree_t *temp = NULL;
 
-	if (!left_insert || !parent)
+	if (!parent || !left_insert)
 		return (NULL);
 	left_insert->n = value;
 	left_insert->right = NULL;
 	left_insert->parent = parent;
-	if (!(parent->left))
-	{
-		parent->left = left_insert;
-		left_insert->left = NULL;
-		return (left_insert);
-	}
-	else
-	{
+	if ((parent->left))
+	{	
 		temp = parent->left;
 		parent->left = left_insert;
 		left_insert->left = temp;
 		temp->parent = left_insert;
 		return (left_insert);
 	}
+	else
+	{
+		parent->left = left_insert;
+		left_insert->left = NULL;
+		return (left_insert);
 
+	}
+
+}
+
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_print(root);
+    printf("\n");
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+    binary_tree_print(root);
+    return (0);
 }
 
 
