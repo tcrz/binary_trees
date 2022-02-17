e8ebc59b7e69a0c7f1477cf2b5489ba94e4e35b5
Tonycrz <tonybaidoo0@gmail.com>
2022-02-17 10:21:16 +0000

    updates

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,20 +17,35 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 	left_insert->n = value;
-	left_insert->left = NULL;
 	left_insert->right = NULL;
 	left_insert->parent = parent;
 	if (!(parent->left))
 	{
 		parent->left = left_insert;
-
+		left_insert->left = NULL;
 	}
 	else
 	{
 		temp = parent->left;
-		parent->left = left_insert;
 		left_insert->left = temp;
-		temp->left = left_insert;
+		temp->parent = left_insert;
+		parent->left = left_insert;
 	}
 	return (left_insert);
+}
+
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_print(root);
+    printf("\n");
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+	binary_tree_insert_left(root, 4);
+    binary_tree_print(root);
+    return (0);
 }
