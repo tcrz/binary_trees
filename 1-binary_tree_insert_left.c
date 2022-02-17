3ca3585a035c4e7ca0eff5f4f72588885d28002f
Tonycrz <tonybaidoo0@gmail.com>
2022-02-17 10:41:09 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	left_insert->right = NULL;
 	left_insert->parent = parent;
 	if ((parent->left))
-	{	
+	{
 		temp = parent->left;
 		parent->left = left_insert;
 		left_insert->left = temp;
@@ -35,19 +35,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 }
 
-int main(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_print(root);
-    printf("\n");
-    binary_tree_insert_left(root->right, 128);
-    binary_tree_insert_left(root, 54);
-    binary_tree_print(root);
-    return (0);
-}
 
 
